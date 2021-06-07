b7cf5822840b16069e954c58f32ed05c8cfb26a6
MOHAMMED SAMEER <84911537+sameer9390@users.noreply.github.com>
2021-06-07 21:39:32 +0530

    Update 4 . Count Character Occurrences using Python.py

--- 4 . Count Character Occurrences using Python.py
+++ 4 . Count Character Occurrences using Python.py
@@ -1,3 +1,10 @@
+''' Counting occurrences of a character in a string means counting all substrings of a character from the input string.  In this , I’ll walk you through how to count character occurrences using Python.
+
+
+ 
+Count Character Occurrences using Python
+To count the occurrences of a character, we need to write an algorithm that returns the number of times each character appears in the input string. The algorithm must iterate through each character from the beginning to count the number of times each character appears in the string. Here’s how to write an algorithm for counting character occurrences using Python:
+'''
 def count_characters(s):
     count = {}
     for i in s:
